9e94cbbe8abdeccabd50659e0c244f69985eeae1
Alejandro CR <contacto@alejandrocr.co>
2025-04-06 00:19:59 -0500

    add httpRequest body for performing HTTP requests

---
+++ tests/requests.js
@@ -0,0 +1,40 @@
+import { request } from 'node:http';
+
+/**
+ * Performs an HTTP request and returns the response.
+ *
+ * @param {import('node:http').RequestOptions & { body?: unknown } | string | URL} options
+ * @returns {Promise<{ statusCode: number, headers: import('node:http').IncomingHttpHeaders, body: Buffer }>}
+ */
+export function httpRequest(options) {
+  return new Promise((resolve, reject) => {
+    const req = request(options);
+
+    if (options.body !== undefined) {
+      req.write(options.body);
+    }
+
+    req.on('response', (res) => {
+      const chunks = [];
+
+      res.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      res.on('end', () => {
+        resolve({
+          statusCode: res.statusCode,
+          statusMessage: res.statusMessage,
+          headers: res.headers,
+          body: Buffer.concat(chunks),
+        });
+      });
+    });
+
+    req.on('error', (e) => {
+      reject(e);
+    });
+
+    req.end();
+  });
+}
