bd11149e4d80027352e3218c00236bf5f4178b3f
Alejandro CR <contacto@alejandrocr.co>
2025-04-06 19:18:12 -0500

    refactor: replace hardcoded access token cookie name with environment variable in jwtGuard

--- src/middlewares.js
+++ src/middlewares.js
@@ -31,13 +31,13 @@ export async function jwtGuard(req, res, next) {
     payload = await verifyToken(token);
     console.log('JWT:', payload);
   } catch (err) {
-    res.clearCookie('access_token');
+    res.clearCookie(env.JWT_COOKIE_NAME);
     return res.status(401).send('Unauthorized 2');
   }
 
   // 3. check payload fields
   if (!payload.sub || !payload.ver || !payload.rex) {
-    res.clearCookie('access_token');
+    res.clearCookie(env.JWT_COOKIE_NAME);
     return res.status(401).send('Unauthorized 3');
   }
 
@@ -47,13 +47,13 @@ export async function jwtGuard(req, res, next) {
   db.close();
 
   if (!user) {
-    res.clearCookie('access_token');
+    res.clearCookie(env.JWT_COOKIE_NAME);
     return res.status(401).send('Unauthorized 4');
   }
 
   // 5. check user jwt version (ver)
   if (user.jwt_version !== payload.ver) {
-    res.clearCookie('access_token');
+    res.clearCookie(env.JWT_COOKIE_NAME);
     return res.status(401).send('Unauthorized 5');
   }
 
